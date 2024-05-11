c5977b73c6b5e60b71044ff44a4066e73c7e24a7
JoeyShapiro <joeyashapiro@gmail.com>
2024-05-10 20:31:31 -0500

    using better audio library

--- NOTES.md
+++ NOTES.md
@@ -1,2 +1,3 @@
 [fancy mouse stuffs](https://stackoverflow.com/questions/3087975/how-to-change-the-cursor-into-a-hand-when-a-user-hovers-over-a-list-item)
 https://github.com/RamiHg/soundio-rs/blob/master/examples/sine/main.rs
+https://github.com/RustAudio/cpal/blob/master/examples/feedback.rs
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,150 +1,95 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-extern crate soundio;
-
-use std::f64::consts::PI;
-
-struct State {
-	// input_stream: soundio::InStream<'static>,
-	output_stream: soundio::OutStream<'static>,
-}
-
-impl State {
-	fn new() -> Result<Self, String> {
-		let mut ctx = soundio::Context::new();
-		ctx.set_app_name("Coconut Player");
-		ctx.connect()?;
-		// We have to flush events so we can scan devices.
-		ctx.flush_events();
-
-		let output_dev = ctx
-			.default_output_device()
-			.map_err(|_| "Error getting default output device".to_string())?;
-
-		println!(
-			"Output device: {} {}",
-			output_dev.name(),
-			if output_dev.is_raw() { "raw" } else { "cooked" }
-		);
-
-		let mut sine = SineWavePlayer {
-			phase: 0.0,
-			amplitude: 0.3,
-			frequency: 200.0,
-		};
-
-		let output_stream = output_dev.open_outstream(
-			48000,
-			soundio::Format::Float32LE,
-			soundio::ChannelLayout::get_default(2),
-			0.5,
-			move |x| sine.write_callback(x),
-			None::<fn()>,
-			None::<fn(soundio::Error)>,
-		)?;
-
-		Ok(Self {
-			// input_stream: soundio::InStream::new(),
-			output_stream,
-		})
-	}
-
-	fn start(&mut self) -> Result<(), String> {
-		self.output_stream.start()?;
-		Ok(())
-	}
-}
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 
 fn main() {
-	// match State::new().un {
-	// 	Ok(_) => ,
-	// 	Err(x) => println!("Error initializing audio: {}", x),
-	// };
-	let mut state = State::new().unwrap();
-	// do run in new thread
-	std::thread::spawn(move || {
-		match run_player() {
-			Ok(_) => (),
-			Err(x) => println!("Error running audio: {}", x),
-		}
-	});
+	#[cfg(any(
+        not(any(
+            target_os = "linux",
+            target_os = "dragonfly",
+            target_os = "freebsd",
+            target_os = "netbsd"
+        )),
+        not(feature = "jack")
+    ))]
+    let host = cpal::default_host();
+
+    // Find devices.
+    let input_device = host.default_input_device()
+    	.expect("failed to find input device");
+
+    let output_device = host.default_output_device()
+    	.expect("failed to find output device");
+
+    println!("Using input device: \"{}\"", input_device.name().unwrap());
+    println!("Using output device: \"{}\"", output_device.name().unwrap());
+
+    // We'll try and use the same configuration between streams to keep it simple.
+    let config: cpal::StreamConfig = input_device.default_input_config().unwrap().into();
+
+    // Create a delay in case the input and output devices aren't synced.
+    let latency_frames = (150.0 / 1_000.0) * config.sample_rate.0 as f32;
+    let latency_samples = latency_frames as usize * config.channels as usize;
+
+    // The buffer to share samples
+    let ring = ringbuf::HeapRb::<f32>::new(latency_samples * 2);
+    let (mut producer, mut consumer) = ring.split();
+
+    // Fill the samples with 0.0 equal to the length of the delay.
+    for _ in 0..latency_samples {
+        // The ring buffer has twice as much space as necessary to add latency here,
+        // so this should never fail
+        producer.push(0.0).unwrap();
+    }
+
+    let input_data_fn = move |data: &[f32], _: &cpal::InputCallbackInfo| {
+        let mut output_fell_behind = false;
+        for &sample in data {
+            if producer.push(sample).is_err() {
+                output_fell_behind = true;
+            }
+        }
+        if output_fell_behind {
+            eprintln!("output stream fell behind: try increasing latency");
+        }
+    };
+
+    let output_data_fn = move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+        let mut input_fell_behind = false;
+        for sample in data {
+            *sample = match consumer.pop() {
+                Some(s) => s,
+                None => {
+                    input_fell_behind = true;
+                    0.0
+                }
+            };
+        }
+        if input_fell_behind {
+            eprintln!("input stream fell behind: try increasing latency");
+        }
+    };
+
+    // Build streams.
+    println!("Attempting to build both streams with f32 samples and `{config:?}`.");
+    let input_stream = input_device.build_input_stream(&config, input_data_fn, err_fn, None).unwrap();
+    let output_stream = output_device.build_output_stream(&config, output_data_fn, err_fn, None).unwrap();
+    println!("Successfully built streams.");
+
+    // Play the streams.
+    println!("Starting the input and output streams with `{}` milliseconds of latency.", 150.0);
+    input_stream.play().unwrap();
+    output_stream.play().unwrap();
 
 	tauri::Builder::default()
-	.run(tauri::generate_context!())
-	.expect("error while running tauri application");
+		.run(tauri::generate_context!())
+		.expect("error while running tauri application");
+
+	drop(input_stream);
+    drop(output_stream);
 }
 
-struct SineWavePlayer {
-    phase: f64, // Phase is updated each time the write callback is called.
-    frequency: f64,
-    amplitude: f64, // TODO: For some reason amplitude close to 1 (maybe > 0.99?) and high frequency (e.g. 18 kHz) gives weird low frequency aliasing or something.
-}
-
-impl SineWavePlayer {
-    fn write_callback(&mut self, stream: &mut soundio::OutStreamWriter) {
-        let mut frames_left = stream.frame_count_max();
-
-        loop {
-            if let Err(e) = stream.begin_write(frames_left) {
-                println!("Error writing to stream: {}", e);
-                return;
-            }
-            let phase_step = self.frequency / stream.sample_rate() as f64 * 2.0 * PI;
-
-            for c in 0..stream.channel_count() {
-                for f in 0..stream.frame_count() {
-                    stream.set_sample(c, f, (self.phase.sin() * self.amplitude) as f32);
-                    self.phase += phase_step;
-                }
-            }
-
-            frames_left -= stream.frame_count();
-            if frames_left <= 0 {
-                break;
-            }
-
-            stream.end_write();
-        }
-    }
-}
-
-fn run_player() -> Result<(), String> {
-	let mut ctx = soundio::Context::new();
-	ctx.set_app_name("Coconut Player");
-	ctx.connect()?;
-	// We have to flush events so we can scan devices.
-	ctx.flush_events();
-
-	let output_dev = ctx
-		.default_output_device()
-		.map_err(|_| "Error getting default output device".to_string())?;
-
-	println!(
-		"Output device: {} {}",
-		output_dev.name(),
-		if output_dev.is_raw() { "raw" } else { "cooked" }
-	);
-
-	let mut sine = SineWavePlayer {
-		phase: 0.0,
-		amplitude: 0.3,
-		frequency: 200.0,
-	};
-
-	let mut output_stream = output_dev.open_outstream(
-		48000,
-		soundio::Format::Float32LE,
-		soundio::ChannelLayout::get_default(2),
-		0.5,
-		move |x| sine.write_callback(x),
-		None::<fn()>,
-		None::<fn(soundio::Error)>,
-	)?;
-
-	output_stream.start()?;
-
-	loop {
-		std::thread::sleep(std::time::Duration::from_secs(1));
-	}
+fn err_fn(err: cpal::StreamError) {
+    eprintln!("an error occurred on stream: {}", err);
 }
