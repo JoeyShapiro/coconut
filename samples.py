7329e2032c0f1e76ecba864834bd514f290543c3
JoeyShapiro <joeyashapiro@gmail.com>
2024-05-11 19:27:06 -0500

    understand how it works

--- samples.py
+++ samples.py
@@ -8,11 +8,20 @@ def in_range(x, y, z):
     return x > y and x < z
 
 # create time graph of data
-sample_rate = 100
-value = [round(float(x.strip())) for x in data[1:] if in_range(x, 0.000001, 10) or in_range(x, -10, -0.000001)][::sample_rate]
+# TODO is this already sampled. how about bitdepth. dont think so
+# i mean, it has to sample it, but its still a lot. maybe not
+# TODO set tick marks
+print(f"{len(data)=} {len(data)/5}")
+# it is sampled. not sure about bit depth though
+sample_rate = 1
+upper = 0.1
+lower = 0.000001
+# oh the rouding of float round(float(x.strip()))
+# TODO make value better. round on output. but dont need to maybe
+value = [float(x.strip()) for x in data[1:] if in_range(x, lower, upper) or in_range(x, -upper, -lower)][::sample_rate][300:400]
 time = [ x for x in range(len(value)) ]
 print(len(value))
 print(f"{max(value)=} {min(value)=}")
 plt.plot(time, value)
+# plt.scatter(time, value)
 plt.show()
-
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -56,25 +56,20 @@ fn main() {
         }
     };
 
-    // create a file
-    let data = "samples\n";
-    let mut f = std::fs::File::create("samples.csv").expect("Unable to create file");
-    std::io::Write::write_all(&mut f, data.as_bytes()).expect("Unable to write data");
+    // TODO like in 270, each person gets a time split. this is know known and used. then they are amplified accordingly
+    // TODO need to change the amplification
 
     // get the current path
     let path = std::env::current_dir().unwrap();
     println!("The current directory is {}", path.display());
 
-    let mut amplifier = 5.0;
+    let mut amplifier = 1.0;
     let output_data_fn = move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
         let mut input_fell_behind = false;
         // fill the output buffer with samples from the ring buffer
         for sample in data {
             *sample = match consumer.pop() {
                 Some(s) => {
-                    // write the sample to the file
-                    std::io::Write::write_all(&mut f, s.to_string().as_bytes()).expect("Unable to write data");
-                    std::io::Write::write_all(&mut f, "\n".as_bytes()).expect("Unable to write data");
                     s*amplifier
                 },
                 None => {
