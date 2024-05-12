f2fc3de29fa545d0d084681bff5d906774e1a160
JoeyShapiro <joeyashapiro@gmail.com>
2024-05-12 09:05:11 -0500

    amplifier can kbe changed

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,18 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::{cell::RefCell, thread::sleep};
+
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 
 fn main() {
-	#[cfg(any(
-        not(any(
-            target_os = "linux",
-            target_os = "dragonfly",
-            target_os = "freebsd",
-            target_os = "netbsd"
-        )),
-        not(feature = "jack")
-    ))]
     let host = cpal::default_host();
 
     // Find devices.
@@ -26,6 +19,7 @@ fn main() {
     println!("Using output device: \"{}\"", output_device.name().unwrap());
 
     // We'll try and use the same configuration between streams to keep it simple.
+    // TODO might be changing valume of eveeryint else
     let config: cpal::StreamConfig = input_device.default_input_config().unwrap().into();
 
     // Create a delay in case the input and output devices aren't synced.
@@ -63,10 +57,17 @@ fn main() {
     let path = std::env::current_dir().unwrap();
     println!("The current directory is {}", path.display());
 
-    let mut amplifier = 1.0;
+    // oh its between threads
+    // create a mutex
+    // cloning a reference?
+    let amplifier = std::sync::Arc::new(std::sync::Mutex::new(1.0));
+    let r = std::sync::Arc::clone(&amplifier);
     let output_data_fn = move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
         let mut input_fell_behind = false;
         // fill the output buffer with samples from the ring buffer
+        // TODO does this do the lock and unlock?
+        let amplifier = { *r.lock().unwrap() };
+        println!("Amplifier: {}", amplifier);
         for sample in data {
             *sample = match consumer.pop() {
                 Some(s) => {
@@ -93,6 +94,15 @@ fn main() {
     println!("Starting the input and output streams with `{}` milliseconds of latency.", 150.0);
     input_stream.play().unwrap();
     output_stream.play().unwrap();
+
+    // sleep for 5s
+    sleep(std::time::Duration::from_secs(5));
+    let r = std::sync::Arc::clone(&amplifier);
+    {
+        let mut amplifier = r.lock().unwrap();
+        *amplifier = 5.0;
+    }
+    println!("Amplifying the audio by 5x");
 
 	tauri::Builder::default()
 		.run(tauri::generate_context!())
