f4a87a163fc06c8f7c1d92e84e8bc747bf43861a
JoeyShapiro <joeyashapiro@gmail.com>
2024-05-25 19:45:53 -0500

    checkpoint: started it fancier with input/output

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::{cell::RefCell, thread::sleep};
+use std::{cell::RefCell, io::Write, os::unix::thread, thread::sleep};
 
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use serde::Serialize;
@@ -18,6 +18,7 @@ struct User {
     name: String,
     pos: Pos,
     is_current: bool, // TODO must be camal case or something 
+    // TODO add amp and stuff, so i dont have to do it every time
 }
 
 #[derive(Serialize, Clone)]
@@ -51,21 +52,23 @@ fn main() {
     let latency_samples = latency_frames as usize * config.channels as usize;
 
     // The buffer to share samples
-    let ring = ringbuf::HeapRb::<f32>::new(latency_samples * 2);
-    let (mut producer, mut consumer) = ring.split();
+    let input_ring = ringbuf::HeapRb::<f32>::new(latency_samples * 2);
+    let (mut input_producer, mut input_consumer) = input_ring.split();
+    let output_ring = ringbuf::HeapRb::<f32>::new(latency_samples * 2);
+    let (mut output_producer, mut output_consumer) = output_ring.split();
 
     // Fill the samples with 0.0 equal to the length of the delay.
     for _ in 0..latency_samples {
         // The ring buffer has twice as much space as necessary to add latency here,
         // so this should never fail
-        producer.push(0.0).unwrap();
+        output_producer.push(0.0).unwrap();
     }
 
     let input_data_fn = move |data: &[f32], _: &cpal::InputCallbackInfo| {
         let mut output_fell_behind = false;
         // push the samples into the ring buffer
         for &sample in data {
-            if producer.push(sample).is_err() {
+            if input_producer.push(sample).is_err() {
                 output_fell_behind = true;
             }
         }
@@ -95,11 +98,12 @@ fn main() {
         } else {
             1.0
         };
+        
         // fill the output buffer with samples from the ring buffer
         for sample in data {
-            *sample = match consumer.pop() {
+            *sample = match output_consumer.pop() {
                 Some(s) => {
-                    s*amp
+                    s * amp
                 },
                 None => {
                     input_fell_behind = true;
@@ -111,6 +115,63 @@ fn main() {
             eprintln!("input stream fell behind: try increasing latency");
         }
     };
+
+    // let tx_data_fn = move |data: &mut [f32]| {
+    //     for sample in data {
+    //         *sample = match input_consumer.pop() {
+    //             Some(s) => s,
+    //             None => 0.0,
+    //         };
+    //     }
+    // };
+
+    let mut oscillator = Oscillator {
+        waveform: Waveform::Sine,
+        sample_rate: config.sample_rate.0 as f32,
+        current_sample_index: 0.0,
+        frequency_hz: 440.0,
+    };
+    let time_at_start = std::time::Instant::now();
+
+    let rx_data_fn = move |data: &[f32], _: &cpal::InputCallbackInfo| {
+        let time_since_start = std::time::Instant::now()
+            .duration_since(time_at_start)
+            .as_secs_f32();
+        if time_since_start < 1.0 {
+            oscillator.set_waveform(Waveform::Sine);
+        } else if time_since_start < 2.0 {
+            oscillator.set_waveform(Waveform::Triangle);
+        } else if time_since_start < 3.0 {
+            oscillator.set_waveform(Waveform::Square);
+        } else if time_since_start < 4.0 {
+            oscillator.set_waveform(Waveform::Saw);
+        } else {
+            oscillator.set_waveform(Waveform::Sine);
+        }
+
+        let mut output_fell_behind = false;
+        // push the samples into the ring buffer
+        for &sample in data {
+            if output_producer.push(oscillator.tick() * 0.3).is_err() {
+                output_fell_behind = true;
+            }
+        }
+        if output_fell_behind {
+            eprintln!("output stream fell behind: try increasing latency");
+        }
+    };
+
+    std::thread::spawn(move || {
+        loop {
+            let data = match input_consumer.pop() {
+                Some(s) => s,
+                None => 0.0,
+            };
+
+            // "send" the data
+
+        }
+    });
 
     // Build streams.
     println!("Attempting to build both streams with f32 samples and `{config:?}`.");
@@ -164,4 +225,106 @@ fn fetch_users() -> Vec<User> {
 
 fn err_fn(err: cpal::StreamError) {
     eprintln!("an error occurred on stream: {}", err);
+}
+
+pub enum Waveform {
+    Sine,
+    Square,
+    Saw,
+    Triangle,
+}
+
+pub struct Oscillator {
+    pub sample_rate: f32,
+    pub waveform: Waveform,
+    pub current_sample_index: f32,
+    pub frequency_hz: f32,
+}
+
+impl Oscillator {
+    fn advance_sample(&mut self) {
+        self.current_sample_index = (self.current_sample_index + 1.0) % self.sample_rate;
+    }
+
+    fn set_waveform(&mut self, waveform: Waveform) {
+        self.waveform = waveform;
+    }
+
+    fn calculate_sine_output_from_freq(&self, freq: f32) -> f32 {
+        let two_pi = 2.0 * std::f32::consts::PI;
+        (self.current_sample_index * freq * two_pi / self.sample_rate).sin()
+    }
+
+    fn is_multiple_of_freq_above_nyquist(&self, multiple: f32) -> bool {
+        self.frequency_hz * multiple > self.sample_rate / 2.0
+    }
+
+    fn sine_wave(&mut self) -> f32 {
+        self.advance_sample();
+        self.calculate_sine_output_from_freq(self.frequency_hz)
+    }
+
+    fn generative_waveform(&mut self, harmonic_index_increment: i32, gain_exponent: f32) -> f32 {
+        self.advance_sample();
+        let mut output = 0.0;
+        let mut i = 1;
+        while !self.is_multiple_of_freq_above_nyquist(i as f32) {
+            let gain = 1.0 / (i as f32).powf(gain_exponent);
+            output += gain * self.calculate_sine_output_from_freq(self.frequency_hz * i as f32);
+            i += harmonic_index_increment;
+        }
+        output
+    }
+
+    fn square_wave(&mut self) -> f32 {
+        self.generative_waveform(2, 1.0)
+    }
+
+    fn saw_wave(&mut self) -> f32 {
+        self.generative_waveform(1, 1.0)
+    }
+
+    fn triangle_wave(&mut self) -> f32 {
+        self.generative_waveform(2, 2.0)
+    }
+
+    fn tick(&mut self) -> f32 {
+        match self.waveform {
+            Waveform::Sine => self.sine_wave(),
+            Waveform::Square => self.square_wave(),
+            Waveform::Saw => self.saw_wave(),
+            Waveform::Triangle => self.triangle_wave(),
+        }
+    }
+}
+
+// data tx / rx
+
+struct Connection {
+    stream: std::net::TcpStream,
+    version: u8,
+    id: u8,
+}
+
+struct Packet {
+    id: u8,
+    data: Vec<f32>,
+}
+
+impl Connection {
+    fn new(version: u8, id: u8) -> Result<Self, std::io::Error> {
+        let stream = std::net::TcpStream::connect("127.0.0.1:34254")?;
+        Ok(Self { stream, version, id })
+    }
+
+    fn rx_data(&mut self) -> Packet {
+        // receive the data
+        Packet { id: 0, data: vec![] }
+    }
+
+    fn tx_data(&mut self, data: Vec<f32>) -> Result<(), std::io::Error> {
+        // send the data
+        self.stream.write(&vec![])?;
+        Ok(())
+    }
 }
