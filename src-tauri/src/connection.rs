46c28716884ecc1c5b331d49b8291290a764c9c4
JoeyShapiro <joeyashapiro@gmail.com>
2024-05-27 09:09:55 -0500

    made multiple files

---
+++ src-tauri/src/connection.rs
@@ -0,0 +1,35 @@
+// data rx / tx
+
+pub struct Connection {
+    // stream: std::net::TcpStream,
+    version: u8,
+    id: u8,
+}
+
+pub struct Packet {
+    id: u8,
+    data: Vec<f32>,
+}
+
+impl Connection {
+    pub fn new(version: u8, id: u8) -> Result<Self, std::io::Error> {
+        // let stream = std::net::TcpStream::connect("127.0.0.1:42069")?;
+        Ok(Self {  version, id })
+    }
+
+    pub fn rx_data(&mut self) -> Vec<Packet> {
+        // receive the data
+        let mut data: Vec<Packet> = vec![];
+        for _ in 0..512 {
+            data.push(Packet { id: 0, data: vec![0.0] });
+        }
+        data
+    }
+
+    pub fn tx_data(&mut self, data: Vec<f32>) -> Result<(), std::io::Error> {
+        // println!("tx: sending data {}", data.len());
+        // send the data
+        // self.stream.write(&vec![])?;
+        Ok(())
+    }
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,16 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+pub mod oscillator;
+pub mod connection;
+
 use std::{cell::RefCell, io::Write, os::unix::thread, process::exit, thread::sleep};
 
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use serde::Serialize;
+
+use crate::oscillator::{Oscillator, Waveform};
+use crate::connection::{Connection, Packet};
 
 struct Settings {
     amplifier: f32,
@@ -284,111 +290,4 @@ fn fetch_users() -> Vec<User> {
 
 fn err_fn(err: cpal::StreamError) {
     eprintln!("an error occurred on stream: {}", err);
-}
-
-pub enum Waveform {
-    Sine,
-    Square,
-    Saw,
-    Triangle,
-}
-
-pub struct Oscillator {
-    pub sample_rate: f32,
-    pub waveform: Waveform,
-    pub current_sample_index: f32,
-    pub frequency_hz: f32,
-}
-
-impl Oscillator {
-    fn advance_sample(&mut self) {
-        self.current_sample_index = (self.current_sample_index + 1.0) % self.sample_rate;
-    }
-
-    fn set_waveform(&mut self, waveform: Waveform) {
-        self.waveform = waveform;
-    }
-
-    fn calculate_sine_output_from_freq(&self, freq: f32) -> f32 {
-        let two_pi = 2.0 * std::f32::consts::PI;
-        (self.current_sample_index * freq * two_pi / self.sample_rate).sin()
-    }
-
-    fn is_multiple_of_freq_above_nyquist(&self, multiple: f32) -> bool {
-        self.frequency_hz * multiple > self.sample_rate / 2.0
-    }
-
-    fn sine_wave(&mut self) -> f32 {
-        self.advance_sample();
-        self.calculate_sine_output_from_freq(self.frequency_hz)
-    }
-
-    fn generative_waveform(&mut self, harmonic_index_increment: i32, gain_exponent: f32) -> f32 {
-        self.advance_sample();
-        let mut output = 0.0;
-        let mut i = 1;
-        while !self.is_multiple_of_freq_above_nyquist(i as f32) {
-            let gain = 1.0 / (i as f32).powf(gain_exponent);
-            output += gain * self.calculate_sine_output_from_freq(self.frequency_hz * i as f32);
-            i += harmonic_index_increment;
-        }
-        output
-    }
-
-    fn square_wave(&mut self) -> f32 {
-        self.generative_waveform(2, 1.0)
-    }
-
-    fn saw_wave(&mut self) -> f32 {
-        self.generative_waveform(1, 1.0)
-    }
-
-    fn triangle_wave(&mut self) -> f32 {
-        self.generative_waveform(2, 2.0)
-    }
-
-    fn tick(&mut self) -> f32 {
-        match self.waveform {
-            Waveform::Sine => self.sine_wave(),
-            Waveform::Square => self.square_wave(),
-            Waveform::Saw => self.saw_wave(),
-            Waveform::Triangle => self.triangle_wave(),
-        }
-    }
-}
-
-// data tx / rx
-
-struct Connection {
-    // stream: std::net::TcpStream,
-    version: u8,
-    id: u8,
-}
-
-struct Packet {
-    id: u8,
-    data: Vec<f32>,
-}
-
-impl Connection {
-    fn new(version: u8, id: u8) -> Result<Self, std::io::Error> {
-        // let stream = std::net::TcpStream::connect("127.0.0.1:42069")?;
-        Ok(Self {  version, id })
-    }
-
-    fn rx_data(&mut self) -> Vec<Packet> {
-        // receive the data
-        let mut data: Vec<Packet> = vec![];
-        for _ in 0..512 {
-            data.push(Packet { id: 0, data: vec![0.0] });
-        }
-        data
-    }
-
-    fn tx_data(&mut self, data: Vec<f32>) -> Result<(), std::io::Error> {
-        // println!("tx: sending data {}", data.len());
-        // send the data
-        // self.stream.write(&vec![])?;
-        Ok(())
-    }
 }
